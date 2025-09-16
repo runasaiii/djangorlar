2297291f522dc9ad79b35d8c55601aa0051f1032
runasaiii <arunakaragulova123@gmail.com>
2025-09-16 13:37:26 +0500

    Started a project

--- djangorlar/djangorlar/settings.py
+++ djangorlar/djangorlar/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ms0o1p)hk*w1(5owc*3#lmu87%89u4$&&gokqvhh_hamxclu8!'
+# SECRET_KEY = 'django-insecure-ms0o1p)hk*w1(5owc*3#lmu87%89u4$&&gokqvhh_hamxclu8!'
 
+SECRET_KEY = config('SECRET_KEY', default='insecure-key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
